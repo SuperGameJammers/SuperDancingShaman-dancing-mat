5cd16b05cb60e75ba725b698ae4042bee7cd9cc4
Dan Amador <danamadorpe@gmail.com>
2016-01-31 09:32:56 -0600

    sockets ain't working

--- Edison/ServerTest.js
+++ Edison/ServerTest.js
@@ -5,12 +5,20 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-app.get('/', function(req, res){
-            io.emit('chat message', "test");
+// Emit welcome message on connection
+io.on('connection', function(socket){
+    console.log('a user connected');
+    io.emit('connection',"hello");
 });
+// Send current time to all connected clients
+function sendTime() {
+    console.log("Sending time");
+    io.emit('time', { time: new Date().toJSON() });
+}
 
+// Send current time every 10 secs
+setInterval(sendTime, 10000);
 
-
-http.listen(3000, function(){
-    console.log('listening on *:3000');
+http.listen(8654, function(){
+    console.log('listening on *:8654');
 });
--- client.js
+++ client.js
@@ -2,19 +2,23 @@ var util = require('util')
 var robot = require("kbm-robot");
 var app = require('express')();
 var http = require('http').Server(app);
-var io = require('socket.io-client');
 
 
-var ip = process.argv[2] || "127.0.0.1";
-var port = process.argv[3] || "8080";
 
-var ioClient = io.connect(formatAddress(ip, port));
+var ip = process.argv[2] || "localhost";
+var port = process.argv[3] || "8654";
 
+//var socket = io.connect(formatAddress(ip, port));
+var io = require('socket.io-client'),
+    socket = io.connect(formatAddress(ip,port));
 function formatAddress(ip, port) {
-    return util.format('https://%s:%s', ip, port);
+    return util.format('https://%s:%s/', ip, port);
 }
-ioClient.on('foo', function (msg) {
-    console.info(msg);
-});
 
-robot.startJar();
+socket.on('connection', function (data) {
+   console.log("FUCK");
+});
+socket.on('time', function(data) {
+console.log(data);
+});
+//robot.startJar();

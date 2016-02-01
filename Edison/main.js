66ff7a15f2ff1308e55643f5ddfc06c76189c23a
Dan Amador <danamadorpe@gmail.com>
2016-02-01 17:30:15 -0600

    small refactor, fixed logic mistake, server finished

--- Edison/main.js
+++ Edison/main.js
@@ -1,21 +1,20 @@
 /**ph
  * Created by damador on 1/31/16.
  */
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
-var m = require('mraa'); //require m
-var playerConnected = false;
-var anal_up = new m.Aio(0), anal_down = new m.Aio(1), anal_left = new m.Aio(2), anal_right = new m.Aio(3);
+var app = require('express')(),
+    http = require('http').Server(app),
+    io = require('socket.io')(http),
+    m = require('mraa'), //require m
+    playerConnected = false,
+    timesPerSec = (1 / 10) * 1000,
+    anal_up = new m.Aio(0), anal_down = new m.Aio(1), anal_left = new m.Aio(2), anal_right = new m.Aio(3),
+    matKeys = [anal_up, anal_down, anal_left, anal_right]; //Anal for everyone :v
 
-var matKeys = [anal_up, anal_down, anal_left, anal_right]; //Anal for everyone :v
-
-function sendTime() {
-    console.log("sending current time, bitch");
-    io.emit('time', {time: new Date().toJSON()});
+function inputCheck() {
+    if (playerConnected == true) {
+        socketFunctions();
+    }
 }
-
-setInterval(sendTime, 5 * 1000);
 
 
 app.get('/', function (req, res) {
@@ -23,34 +22,33 @@ app.get('/', function (req, res) {
 });
 
 
-function button_press(matKeys) {
+function button_press() {
     for (var i = 0; i < matKeys.length; i++) {
-        if (matKeys[i].read() > 500) return i;//Circuit is kind of screwed, so it sometimes outputserroneous values.
+        if (matKeys[i].read() > 500) {
+            return i;//Circuit is kind of screwed, so it sometimes outputs erroneous values.
+        }
     }
     return 5;
 }
 
 io.on('connection', function (socket) {
-    console.log("player connected");
-    if (playerConnected) socketFunctions(socket);
-
-    var key = button_press();
-    if (key != 5) sendKeyValue(socket, key);
+    playerConnected = true;
+    //setInterval(sendTime, 5 * 1000);
+    setInterval(inputCheck, timesPerSec)
+    //setInterval(inputCheck(socket), (Math.floor((1 / timesPerSec) * 1000)));
     socket.on('disconnect', function () {
         console.log('user disconnected');
         playerConnected = false;
     });
-    playerConnected = true;
 });
 
-function sendKeyValue (socket, key) {
-    socket.emit('input', key);
+function sendKeyValue(key) {
+    io.emit('input', key);
 };
 
-function socketFunctions(socket) {
-    if (dig_down.read()) {
-        socket.emit('input')
-    }
+function socketFunctions() {
+    var value = button_press();
+    if (value != 5) sendKeyValue(value);
 }
 
 http.listen(8080, function () {
